66c387f589954caa1b83eec35d311cd9f845e10b
Mike Chadwick <mikechadwick@Mikes-MacBook-Pro.local>
2020-03-05 14:20:36 -0700

    h

--- practice.js
+++ practice.js
@@ -17,8 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -32,8 +33,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
+function last(arr){
+  return arr.pop()
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -47,7 +49,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family) {
+for(let i=0; i < family.length; i++){
+  alert(family[i])
+}
+}
+
 
 
 
